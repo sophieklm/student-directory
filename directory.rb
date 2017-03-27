9ec134ad4f4b2a3411ac178935a1d441b192d664
sophieklm <sklm@live.co.uk>
2017-03-27 12:12:36 +0100

    string interpolation

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #first print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,7 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 #print the total number of students
-print "Overall, we have "
-#print doen't add new line characters
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
