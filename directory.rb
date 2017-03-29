33f03da25be4aedc307abec49891a66e20367297
sophieklm <sklm@live.co.uk>
2017-03-29 13:40:54 +0100

    ex12 load student list from file

--- directory.rb
+++ directory.rb
@@ -16,6 +16,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -27,6 +28,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9" #terminate program
     exit
   else
@@ -52,6 +55,15 @@ def save_students
     student_data = [student[:name], student[:cohort], student[:country], student[:hobby]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, country, hobby = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym, country: country, hobby: hobby}
   end
   file.close
 end
